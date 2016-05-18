eb343efd3769bc8937166f44d0f0bf08ffeb5ba8
jthelin <jthelin@microsoft.com>
2016-05-17 18:31:32 -0700

    Probe for python tool locations.

--- Env-Info.cmd
+++ Env-Info.cmd
@@ -9,13 +9,34 @@ set CMDHOME=%CMDHOME:~0,-1%
 @echo ==========================
 set
 
+@echo .
 @echo Locate shells and programs
 @echo ==========================
-@for %%p in (cmd bash java javac scala python ant mvn sbt nuget choco scoop curl wget) do (
+@for %%p in (cmd bash java javac scala python pip easy_install dnx csc cmake ant mvn sbt msbuild nuget choco scoop curl wget) do (
   @echo %%p
   where %%p
 )
 
+@echo .
+@echo Show top level directories
+@echo ==========================
+dir C:\
+
+@echo .
+@echo Show Python files
+@echo =================
+dir C:\python27
+dir C:\python27\scripts
+
+@echo .
+@echo Show Java tools
+@echo ===============
+dir C:\java
+for /d %%D in (C:\java\*) do (
+  dir %%D
+)
+
+@echo .
 if "%AGENT_HOMEDIRECTORY%" == "" (
   @echo ==== Not running on VSO worker node ====
 ) else (
