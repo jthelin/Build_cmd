dd49529a6195ac152c12442ea90417a95209be73
Jorgen Thelin <jthelin@microsoft.com>
2017-10-23 12:00:17 -0700

    ListHostSpec.ps1
    - List the machine specification for the [build server] host we are running on.

---
+++ ListHostSpec.ps1
@@ -0,0 +1,20 @@
+# List the machine specification for the host we are running on.
+#
+# Based on: https://sabin.io/blog/vsts-hosted-build-specs-the-script/
+#
+
+$cpu  = Get-WmiObject Win32_Processor
+$os   = Get-WmiObject Win32_OperatingSystem
+$bios = Get-WmiObject Win32_Bios
+$mem  = Get-WmiObject CIM_PhysicalMemory | Measure -Property Capacity -Sum | % { [Math]::Round(($_.sum / 1GB), 2) }
+
+Foreach ($c in $cpu)
+{
+    "CPU Name        : " + $c.Name
+    "Physical Cores  : " + $c.NumberOfCores
+    "Logical  Cores  : " + $c.NumberOfLogicalProcessors
+    "L3 Cache Memory : " + $c.L3CacheSize + " KB"
+    "Physical Memory : " + $mem + " GB"
+    "BIOS Version    : " + $bios.Manufacturer + " " + $bios.SMBIOSBIOSVersion
+    "OS Version      : " + $os.Version
+}
