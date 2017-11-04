0a332ebf77f8522ea3637696aa3e81984d69ba1b
Jorgen Thelin <jthelin@microsoft.com>
2017-11-04 00:31:40 -0700

    Install DotNet

---
+++ install-dotnet-apt-pkg.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Install .NET Core 2.0 apt-get package.
+#
+# https://www.microsoft.com/net/learn/get-started/linuxubuntu
+#
+
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+rm microsoft.gpg
+
+# Ubuntu 16.04 / Linux Mint 18
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-$(lsb_release -cs)-prod $(lsb_release -cs) main" > /etc/apt/sources.list.d/dotnetdev.list'
+
+sudo apt-get update
+sudo apt-get install dotnet-sdk-2.0.2
