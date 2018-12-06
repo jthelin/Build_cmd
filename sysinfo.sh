fbcef9fa80da0a2c4706fc3d3313046c1c6fe54b
Jorgen Thelin <jthelin@microsoft.com>
2018-12-06 15:48:28 -0800

    sysinfo.sh - Dump some Linux system info, including NVIDIA GPUs details.

---
+++ sysinfo.sh
@@ -0,0 +1,53 @@
+echo System configuration info for host $HOSTNAME
+
+echo .
+echo uname
+echo .
+uname -a
+
+echo .
+echo os-release
+echo .
+cat /etc/os-release
+
+echo .
+echo lscpu
+echo .
+lscpu
+
+# echo .
+# echo meminfo
+# echo .
+# cat /proc/meminfo
+
+echo .
+echo Show NVIDIA Kernel Modules and Packages Installed
+echo .
+
+( lspci | grep -i NVIDIA ) || echo "WARNING: No NVIDIA modules found."
+
+for pkg in NVIDIA CUDA NCCL
+do
+    echo .
+    echo "$pkg Packages"
+    dpkg-query --list "*${pkg,,*}*"
+done
+
+if [ `which nvidia-smi` ]
+then
+
+echo .
+echo nvidia-smi
+echo .
+nvidia-smi
+
+echo .
+echo nvidia-topo
+echo .
+nvidia-smi topo -m
+
+else
+    echo !
+    echo "WARNING: nvidia-smi app was not found."
+    echo !
+fi
