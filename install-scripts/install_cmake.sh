cc3cdeebf74f5029a5728d3686f024a20c0e9c8c
Jorgen Thelin <jthelin@microsoft.com>
2020-01-14 19:24:02 -0800

    Support installation under designated location - default to /opt
    Install cmake v3.15.3 to match the version included with Visual Studio 2019
    
    Validate SHA256 hash of cmake download before installing.

--- install-scripts/install_cmake.sh
+++ install-scripts/install_cmake.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
+if [ "$1" != "" ]; then
+  INSTALL_ROOT=$1
+else
+  INSTALL_ROOT=/opt
+fi
+
+# This is the same version of cmake included in Visual Studio 2019
+CMAKE_VERSION="3.15.3"
+
 # Download info: https://github.com/Kitware/CMake/releases
 
-CMAKE_VERSION="3.13.2"
+CMAKE_HASH_SHA256="020812a9f87293482cec51fdf44f41cc47e794de568f945a8175549d997e1760"
 CMAKE_TARFILE="cmake-${CMAKE_VERSION}-Linux-x86_64.tar.gz"
 CMAKE_DOWNLOAD="https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/${CMAKE_TARFILE}"
-CMAKE_INSTALL="/opt/cmake"
-
-CMAKE_RELEASE_PGP_SIG="EC8FEF3A7BFB4EDA"
-# http://pool.sks-keyservers.net:11371/pks/lookup?op=get&search=0xCBA23971357C2E6590D9EFD3EC8FEF3A7BFB4EDA
-
-gpg --receive-keys "${CMAKE_RELEASE_PGP_SIG}"
-
-set -x
+CMAKE_INSTALL="${INSTALL_ROOT}/cmake"
 
 curl -fsSL -o "${CMAKE_TARFILE}" "${CMAKE_DOWNLOAD}"
 
-mkdir -p "${CMAKE_INSTALL}" && \
-tar -xf "${CMAKE_TARFILE}" --strip 1 -C "${CMAKE_INSTALL}"
+echo "Installing cmake v${CMAKE_VERSION} to ${CMAKE_INSTALL}"
+
+sudo mkdir -p "${CMAKE_INSTALL}" && \
+echo "${CMAKE_HASH_SHA256} ${CMAKE_TARFILE}" | sha256sum --check - && \
+echo "Downloaded file integrity checked OK." && \
+sudo tar -xf "${CMAKE_TARFILE}" --strip 1 -C "${CMAKE_INSTALL}"
