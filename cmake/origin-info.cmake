f7e0d926ec13bd0e2422479289af12601551d713
jthelin <jthelin@microsoft.com>
2020-12-09 16:42:53 -0800

    origin-info.cmake - env vars
    - Also read env variables in origin-info.cmake
    
    - Also output JETBRAINS_* variables.

--- cmake/origin-info.cmake
+++ cmake/origin-info.cmake
@@ -13,8 +13,11 @@ function(record_origin_info output_file)
 
     set(variable_name_match_patterns
         "^BUILD_.*"
-        "^CMAKE_BUILD_.*")
+        "^CMAKE_BUILD_.*"
+        "^JETBRAINS_.*"
+        )
 
+    # cmake variables
     get_cmake_property(variable_names VARIABLES)
     list(SORT variable_names)
     foreach(varname ${variable_names})
@@ -24,4 +27,31 @@ function(record_origin_info output_file)
             endif()
         endforeach()
     endforeach()
+
+    # Runtime environment variables
+    if(WIN32)
+        set(CMD_STR cmd /c set)
+    else()
+        set(CMD_STR bash -c "env | sort")
+    endif()
+    execute_process(
+        COMMAND ${CMD_STR}
+        WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+        OUTPUT_VARIABLE list_env_vars
+        COMMAND_ECHO STDOUT
+        ECHO_OUTPUT_VARIABLE
+    )
+    string(REPLACE ";" "[--COMMA--]" list_env_vars "${list_env_vars}")
+    string(REPLACE "\n" ";" list_env_vars "${list_env_vars}")
+    foreach(line IN LISTS list_env_vars)
+        string(REPLACE "[--COMMA--]" ";" line "${line}")
+        foreach(pattern ${variable_name_match_patterns})
+            if(line MATCHES ${pattern})
+                file(APPEND ${output_file} "${line}\n")
+            endif()
+        endforeach()
+    endforeach()
+
+    file(APPEND ${output_file} "\n")
+    message(STATUS "Wrote origin-info to ${output_file}")
 endfunction()
--- info-scripts/origin-info.sh
+++ info-scripts/origin-info.sh
@@ -9,7 +9,9 @@ trap "rm -f ""${TMP_ENV_VAR_LIST}"" ""${TMP_ORIGIN_INFO}"" " 0 2 3 15
 env | sort >"${TMP_ENV_VAR_LIST}"
 
 echo "HOSTNAME=$(hostname)" >"${TMP_ORIGIN_INFO}"
-grep --ignore-case "^BUILD_" <"${TMP_ENV_VAR_LIST}" >>"${TMP_ORIGIN_INFO}"
-grep --ignore-case "^CMAKE_BUILD_" <"${TMP_ENV_VAR_LIST}" >>"${TMP_ORIGIN_INFO}"
+for pattern in '^BUILD_.*' '^CMAKE_BUILD_.*' '^JETBRAINS_.*'
+do
+    grep --ignore-case "${pattern}" <"${TMP_ENV_VAR_LIST}" >>"${TMP_ORIGIN_INFO}"
+done
 
 cat "${TMP_ORIGIN_INFO}"
